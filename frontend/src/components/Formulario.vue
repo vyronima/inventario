26e3e4493489989eca3abea4bc10be2a7765e79c
vyromina <anllyver23@gmail.com>
2024-11-26 22:51:28 -0500

    .

--- frontend/src/components/Formulario.vue
+++ frontend/src/components/Formulario.vue
@@ -1,32 +1,18 @@
 <template>
-
-
 <div class="container_header">
     <h2 class="titulo">Modulo de {{titulo}}</h2>
     <el-row justify="center" align="middle">
         <el-button type="Danger">Cancelar</el-button>
         <el-button type="Danger">Guardar</el-button>
-
     </el-row>
-
     <slot name="slotForm" ></slot>
     </div>
-
-
-
 </template>
-
-
-
 <script setup>
-
-defineProps({
+  defineProps({
   titulo: String,
   
 })
-
 </script>
-
-
-<style scoped>
+  <style scoped>
 </style>
