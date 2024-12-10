dfaa3c09dcf4b7fdd78829a8bc0215ccfa77f6fb
vyronima <ANLLYVER23@GMAIL.COM>
2024-12-09 23:11:38 -0500

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,3 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-export default defineConfig({
-  plugins: [vue()],
-  base: '/', // Cambia esto si tu aplicación está en un subdirectorio
-  server: {
-    proxy: {
-      '/productos': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
-})
+module.exports = {
+    publicPath: '/',
+  };
